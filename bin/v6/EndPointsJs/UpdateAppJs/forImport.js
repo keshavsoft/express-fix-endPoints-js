a5208011b6a0415ba7af13a3764af70c25ecf3ae
keshavsoft <nknnkumar@yahoo.com>
2026-06-01 10:06:35 +0530

    as json

--- bin/v6/EndPointsJs/UpdateAppJs/forImport.js
+++ bin/v6/EndPointsJs/UpdateAppJs/forImport.js
@@ -4,8 +4,19 @@ import fs from "fs";
 
 import updateImports from "./common/AlterFile/index.js";
 import validateEndpoint from "./validations/validateEndpoint.js";
+import importJson from "./forImport.json" with {type: "json"};
 
 const buildLinesForImport = (endpoint) => {
+    return {
+        importLine: importJson.importLine.replaceAll("${endpoint}", endpoint).replaceAll("'", '"'),
+        duplicationCheck: importJson.duplicationCheck.replaceAll("${endpoint}", endpoint).replaceAll("'", '"'),
+        importInsertAfter: importJson.insertAfter.replaceAll("'", '"')
+    };
+};
+
+const buildLinesForImport1 = (endpoint) => {
+
+
     const importLine = `import funcFrom${endpoint} from "./${endpoint}/controller.js";`;
     const duplicationCheck = `from "./${endpoint}/controller.js"`;
 

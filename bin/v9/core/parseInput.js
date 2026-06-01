bf5e19fa8237c892e4a526642f00bb4c2d823dd2
keshavsoft <nknnkumar@yahoo.com>
2026-06-01 11:34:21 +0530

    9

---
+++ bin/v9/core/parseInput.js
@@ -0,0 +1,14 @@
+export default function parseInput({ endPointsJsPath, actionName,
+    inCheckLines = {}, showLog }) {
+
+    const [...args] = process.argv.slice(2);
+
+    return {
+        actionName: args[0] || actionName,
+        showLog: args[1] === undefined
+            ? showLog
+            : args[1] === "true",
+        endPointsJsPath: endPointsJsPath || process.cwd(),
+        inCheckLines
+    };
+};

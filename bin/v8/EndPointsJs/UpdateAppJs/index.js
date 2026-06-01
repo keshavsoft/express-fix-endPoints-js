8d034e36c622109ce6830e5281ec8cca3f61d26b
keshavsoft <nknnkumar@yahoo.com>
2026-06-01 10:40:00 +0530

    8

---
+++ bin/v8/EndPointsJs/UpdateAppJs/index.js
@@ -0,0 +1,59 @@
+// v2/AppJs/index.js
+
+import checkLines from "./checkLines.json" with {type: "json"};
+import validateEndpoint from "./validations/validateEndpoint.js";
+import alterFile from "./common/AlterFile/index.js";
+
+const repalceLines = ({ inCheckLines, inEndpoint }) => {
+    return {
+        importLine: inCheckLines.toInsertLine.replaceAll("${endpoint}", inEndpoint).replaceAll("'", '"'),
+        duplicationCheck: inCheckLines.duplicationCheck.replaceAll("${endpoint}", inEndpoint).replaceAll("'", '"'),
+        importInsertAfter: inCheckLines.insertAfter.map(element => {
+            return element.replaceAll("'", '"')
+        })
+    };
+};
+
+const repalceFullJson = ({ inCheckLines, inEndpoint }) => {
+    return {
+        importLines: repalceLines({
+            inCheckLines: inCheckLines.importLines,
+            inEndpoint
+        }),
+        useLines: repalceLines({
+            inCheckLines: inCheckLines.useLines,
+            inEndpoint
+        })
+    };
+};
+
+const updateAppJs = ({ inEndPointsJsPath, actionName,
+    showLog = false }) => {
+
+    validateEndpoint({ endpoint: actionName });
+
+    const checkLinesAltered = repalceFullJson({
+        inCheckLines: checkLines,
+        inEndpoint: actionName
+    });
+
+    alterFile({
+        jsFilePath: inEndPointsJsPath,
+        importLine: checkLinesAltered.importLines.importLine,
+        duplicationCheck: checkLinesAltered.importLines.duplicationCheck,
+        importInsertAfter: checkLinesAltered.importLines.importInsertAfter,
+        showLog
+    });
+
+    alterFile({
+        jsFilePath: inEndPointsJsPath,
+        importLine: checkLinesAltered.useLines.importLine,
+        duplicationCheck: checkLinesAltered.useLines.duplicationCheck,
+        importInsertAfter: checkLinesAltered.useLines.importInsertAfter,
+        showLog
+    });
+
+    return false;
+};
+
+export default updateAppJs;

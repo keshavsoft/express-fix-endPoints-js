ee8ea1fb890e80793ba874b72e99889eddb8c8bf
keshavsoft <nknnkumar@yahoo.com>
2026-06-15 11:57:51 +0530

    var changed to endPointsJsPath

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import getLatestVersion from "./bin/core/getLatestVersion.js";
 
-const load = async ({ jsFilePath, inActionName, inCheckLines, showLog }) => {
+const load = async ({ endPointsJsPath, inActionName, inCheckLines, showLog }) => {
     const v = getLatestVersion();
 
     const module = await import(`./bin/${v}/start.js`);
 
-    await module.default({ jsFilePath, inActionName, inCheckLines, showLog });
+    await module.default({
+        jsFilePath: endPointsJsPath,
+        inActionName, inCheckLines, showLog
+    });
 };
 
 export default load;
